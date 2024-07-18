90f565ed72e43bcfdac4fc604da7c3191abc0873
priyanka singh <priyanka.singh.2907.2k.com>
2024-07-18 15:14:03 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import Home from "./components/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Designs from "./components/Designs.jsx";
+import BuySell from "./components/BuySell.jsx";
+import Blogs from "./components/Blogs.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./components/Home.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Designs />
+      <BuySell />
+      <Blogs />
+      <Footer />
+    </div>
+  );
+}
