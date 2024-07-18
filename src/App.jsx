e2868866ede8e871307189d424c8f07c3d1afaf7
priyanka singh <priyanka.singh.2907.2k.com>
2024-07-18 17:06:52 +0530

    frontend updated.

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,39 @@ import BuySell from "./components/BuySell.jsx";
 import Blogs from "./components/Blogs.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./components/Home.jsx";
-import scrollreveal from "scrollreveal";
-import useEffect from "react";
+// import scrollreveal from "scrollreveal";
+// import useEffect from "react";
 
 export default function App() {
-  useEffect(() => {
-    const registerAnimations = () => {
-      const sr = scrollreveal({
-        origin: "bottom",
-        distance: "80px",
-        duration: 2000,
-        reset: false,
-      });
-      sr.reveal(
-        ` nav,
-          #home,
-          #buySell1,
-          #buySell2,
-          #buySell3,
-          #blogs,
-          footer
-      `,
-        {
-          opacity: 0,
-          interval: 500,
-        }
-      );
-    };
-    registerAnimations();
-  }, []);
+  // useEffect(() => {
+  //   const registerAnimations = () => {
+  //     const sr = scrollreveal({
+  //       origin: "bottom",
+  //       distance: "80px",
+  //       duration: 2000,
+  //       reset: true,
+  //     });
+  //     sr.reveal(
+  //       ` nav,
+  //         #home,
+  //         #buySell1,
+  //         #buySell2,
+  //         #buySell3,
+  //         #blogs,
+  //         footer
+  //     `,
+  //       {
+  //         opacity: 1,
+  //         interval: 500,
+  //       }
+  //     );
+  //   };
+  //   registerAnimations();
+  // }, []);
+  window.setTimeout(() => {
+    const home = document.getElementById("home");
+    home.style.transform = "none";
+  }, 1500);
 
   return (
     <div>
