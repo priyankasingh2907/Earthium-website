d3a99b701fd88bb71bf7bce255d7c30b7a01914a
priyanka singh <priyanka.singh.2907.2k.com>
2024-07-18 16:48:50 +0530

    frontend updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,36 @@ import BuySell from "./components/BuySell.jsx";
 import Blogs from "./components/Blogs.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./components/Home.jsx";
+import scrollreveal from "scrollreveal";
+import useEffect from "react";
 
 export default function App() {
+  useEffect(() => {
+    const registerAnimations = () => {
+      const sr = scrollreveal({
+        origin: "bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false,
+      });
+      sr.reveal(
+        ` nav,
+          #home,
+          #buySell1,
+          #buySell2,
+          #buySell3,
+          #blogs,
+          footer
+      `,
+        {
+          opacity: 0,
+          interval: 500,
+        }
+      );
+    };
+    registerAnimations();
+  }, []);
+
   return (
     <div>
       <Navbar />
